3b658c3a38e5166406bccad1bab5a38df81a558a
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-08-13 15:05:19 +0000

    story

---
+++ story.py
@@ -0,0 +1,20 @@
+def main():
+    name = input("What is your name? ")
+    color = input("Pick a color: ")
+    adjective = input("Give me an adjective: ")
+    goal = input("A goal you would like to achieve: ")
+
+
+    # Formatted Strings
+    print(f"")
+    print(f"Hello {name}!")
+    print(f"")
+    print(f"This is your story: ")
+    print(f"  At Uranus the people are the color {color},\n and they are very {adjective}.\n They all randomly decided they would {goal}.")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
