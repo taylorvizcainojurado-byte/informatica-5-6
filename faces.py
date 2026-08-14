c2a5dde198f3041e71c2e093b32cf780452797f8
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-08-14 15:27:27 +0000

    emojis

---
+++ faces.py
@@ -0,0 +1,36 @@
+def main():
+    face = input(" ")
+    face = face.replace(":)","🙂",count=-1)
+    face = face.replace(":(","🙁",count=-1)
+    print(face)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
