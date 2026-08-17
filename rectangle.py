00759347f52ccaacc1bfaca9c60f9fd63123f153
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-08-17 14:55:17 +0000

    rectangularprism

---
+++ rectangle.py
@@ -0,0 +1,15 @@
+def main():
+
+    F = int(input("Rectangle Width: "))
+
+    h = int(F) * "0"
+    print(f"{h}")
+    print(f"{h}")
+    print(f"{h}")
+    print(f"{h}")
+    print(f"{h}")
+
+    8
+    
+if __name__ == "__main__":
+    main()
