5ad9832b982052d3b82dc6875dbd8c1481a970b4
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-08-19 18:00:07 +0000

    assignment operator

---
+++ update-variable.py
@@ -0,0 +1,32 @@
+def main():
+    #augmented assignment operator
+    score = 10
+    score += 10
+    print(score)
+
+    #subtraction assignment operator
+    sub = 100
+    sub -= 15
+    print(sub)
+
+    #multiplication assignment operator
+    mult = 10
+    mult *= 4
+    print(mult)
+
+    #division assignment operator
+    div = 10
+    div /= 4
+    print(div)
+
+    #modulo assignment operator
+    a = 10
+    b = 4
+    a %= b
+    print(a)
+
+    #I PRESENTED BTW
+
+
+if __name__ == "__main__":
+    main()
