e66206e7967e0b7d10b1d0285739ed39d910e1df
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-08-20 15:36:21 +0000

    qr-code

---
+++ qr-code.py
@@ -0,0 +1,16 @@
+import qrcode
+
+def main():
+    song = "https://www.youtube.com/watch?v=Om_EJqTsqLY&list=RDOm_EJqTsqLY&start_radio=1"
+    qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
+    qr.add_data(song)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color = "green", back_color = "black")
+    img.save("youtube-qr.png")
+
+
+
+if __name__ == "__main__":
+    main()
+
