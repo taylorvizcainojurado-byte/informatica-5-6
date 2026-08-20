e92fe69e8d54602e802643024f8e4da895070184
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-08-20 15:47:53 +0000

    qr-code new codspace

--- qr-code.py
+++ qr-code.py
@@ -1,7 +1,7 @@
 import qrcode
 
 def main():
-    song = "https://www.youtube.com/watch?v=Om_EJqTsqLY&list=RDOm_EJqTsqLY&start_radio=1"
+    song = "https://www.youtube.com/watch?v=6wczvHl2vSM&list=RD6wczvHl2vSM&start_radio=1"
     qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
     qr.add_data(song)
     qr.make(fit=True)
