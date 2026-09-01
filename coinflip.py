40a488882816407594c08dc3a4589583209aa127
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-01 21:46:38 +0000

    coinflip

--- coinflip.py
+++ coinflip.py
@@ -1,19 +1,24 @@
 import random
+def main():
 
-ges = int(input("Heads(1) or Tails(2): "))
+    coin = ["heads","tails"]
+    attempts = 3
+    while attempts > 0:
+        flip = random.choice(coin)
+        ges = input("Heads or Tails: ").strip().lower()
 
-otcm = random.randint(1,2)
-
-if otcm == 1:
-    print("Heads")
-elif otcm == 2:
-    print("Tails")
-else:
-    print("Not an Option")
-
-if ges == otcm:
-    print("You Win!")
-else:
-    print("You Lose!")
+        print("The coin landed on", flip)
 
 
+
+        if ges == flip:
+            print("You Win!")
+            break
+        else:
+            print("You Lose!")
+            attempts -= 1
+            print("Attempts left:", attempts)
+
+
+if __name__ == "__main__":
+    main()
