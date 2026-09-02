c3c30ff4002325314eded8d6629618c165b0f69a
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-02 17:54:10 +0000

    are we there yet

---
+++ roadtrip.py
@@ -0,0 +1,12 @@
+def main():
+    answer = ""
+
+    while answer != "Yes!":
+        answer = input("Are we there yet? ").strip().title()
+    print("We are here! ")
+
+
+
+
+if __name__ == "__main__":
+    main()
