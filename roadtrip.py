1df571e82fb8a2dfadc81bfacb01c551e7ff2a43
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-02 17:59:44 +0000

    road

--- roadtrip.py
+++ roadtrip.py
@@ -3,6 +3,12 @@ def main():
 
     while answer != "Yes!":
         answer = input("Are we there yet? ").strip().title()
+        if answer == "Yes":
+            followup = input("Really? ").title().strip()
+        if followup == "Yes":
+            break
+        else:
+            print("I hate you gng ")
     print("We are here! ")
 
 
