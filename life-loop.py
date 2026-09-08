0501c2f898ca437109761ec3c74b045fd3fd662a
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-08 20:19:43 +0000

    kajef

--- life-loop.py
+++ life-loop.py
@@ -1,25 +1,28 @@
 import random
-import time
+
 
 def main():
     ideas = ["Cutural Backround", "Lessons from failure", "Personal Challenges", "Debates on tecnology", "Education System", "Historical turning points", "Psychology concepts", "Literature", "Historical Speeches"]
+    rndm = random.choice(ideas)
     print("Cant think of ideas for your essay? ")
     help = input("Do you need help? ").lower().strip()
     if help == "yes":
         print("Okay I got your back! ")
     else:
         print("Im gonna help you anyway ")
-    rndm = random.choice(ideas)
+
     print("Heres an idea that might help you out ")
     print(rndm)
     help2 = input("Does this help? ").lower().strip()
     while help2 != "yes":
-        if help2 == "yes":
-            print("Glad I could help! ")
-            break
-        else:
-            print("Heres another idea ")
+        help3 = input("Want another idea? ").lower().strip()
+        if help3 == "yes":
+            print("Heres another idea! ")
             print(rndm)
+
+        else:
+            print("Glad I could Help! ")
+            break
 
 
 
