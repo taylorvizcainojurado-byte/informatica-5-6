b9dbbed2640a840c9493466566cabc64c4abc69f
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-18 20:15:53 +0000

    gospel doctrine

---
+++ week5/for-intro.py
@@ -0,0 +1,15 @@
+def main():
+    doctrine = ["Baptism", "Faith", "Holy Ghost", "Repentance", "Endure to the End"]
+    num = 1
+    for jesus in doctrine:
+        print(f"{num}: {jesus}")
+        num += 1
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
