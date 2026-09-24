2609edfef10598e5b6515b4199c77b60557bbe6b
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-24 14:40:16 +0000

    table2

---
+++ week6/tables2.py
@@ -0,0 +1,13 @@
+def main():
+    table = 5
+
+    print(f"Here is the {table} times table")
+
+    for x in range(10,30):
+        ans = x * table
+        print(f"{x} times {table} is {ans}")
+
+
+if __name__ == "__main__":
+    main()
+    
