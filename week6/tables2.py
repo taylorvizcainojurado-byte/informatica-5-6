2205e8432a0171e8d5ed201226621e78ce6dbab3
taylorvizcainojurado-byte <taylorvizcainojurado@gmail.com>
2026-09-24 15:22:21 +0000

    table2good

--- week6/tables2.py
+++ week6/tables2.py
@@ -1,6 +1,9 @@
 def main():
+    valid = []
+    for i in range(1,11):
+        valid.append(str(i))
     while True:
-        table = input("Enter a number: ").lower().strip()
+        table = input("Enter a number (1-10 or exit): ").lower().strip()
         if table == "exit":
             break
         else:
@@ -12,6 +15,9 @@ def main():
             for x in range(1,max+1):
                 ans = x * int(table)
                 print(f"{x} times {table} is {ans}")
+            else:
+                print("Invalid command")
+
 
 
 if __name__ == "__main__":
